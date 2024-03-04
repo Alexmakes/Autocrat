12b1253ce039e0c749e86ce87c6ba615be8d0993
Fizzy wizard <alex@alexmakes.net>
2024-03-04 19:46:05 +0000

    got keyboard input working on press of q
    also fixed the prompt file

--- llmapi.py
+++ llmapi.py
@@ -1,16 +1,18 @@
 # Example: reuse your existing OpenAI setup
 import os
 from openai import OpenAI
-from tkinter import *
-import tkinter.messagebox as messagebox
+#from tkinter import *
+#import tkinter.messagebox as messagebox
+import keyboard
+import random
 
 # declaring varibles 
 
-#global my_listbox
 llmpreprompt = "In a single short paragraph. you are British and grumpy a petty person of no worth and you can only respond in passive being aggressive statements your life goal is to is too be petty over small civil matters. You find pleasure in complaining to the local council of Stockport and they have not being responding to your letter and this is your final straw and tell them how you really feel."
-llmmessege_f = "i would like to you to write a complaint about {}"
+llmmessege_f = "i would like to you to write a complaint about "
 
-llmmessege = ""
+#llmmessege = ""
+
 
 
 def getllmmessege():
@@ -28,6 +30,7 @@ def getllmmessege():
   )
 
   print(completion.choices[0].message)
+  llmmessege = ""
 
 
 
@@ -36,14 +39,15 @@ def on_click():
     global llmmessege_f # set global for llmessege_f
     selection = my_listbox.get(my_listbox.curselection())  # gets the current selected item from the listbox
     llmmessege = llmmessege_f.format([selection]) # formats the varible and inserts the user selection from a list
-    messagebox.showinfo("Selected", "You have selected: " + str(selection))
     getllmmessege()
 
+"""
 def pick_random():
     random_item = random.choice(my_list)
     my_listbox.insert(END, random_item) # adds a new item to the listbox
     messagebox.showinfo("Random Item", "A random line has been added: " + str(random_item)) 
 
+ 
 def on_cancel():
     root.destroy()
 
@@ -70,7 +74,35 @@ def gui():
   cancel_button.pack()
 
   root.mainloop()
+"""
 
-gui()
-getllmmessege()
+def random_item_from_array(array):
+    return array[random.randrange(0,len(array),1)]
+
+def read_file(promtfile):
+    try:
+        with open(promtfile, 'r') as rawfile:
+            return sorted([line.strip() for line in rawfile])
+    except FileNotFoundError:
+        raise Exception("File not found")
+
+
+while True:  # making a loop
+      # used try so that if user pressed other than the given key error will not be shown
+        keyboard.wait('q')  # if key 'q' is pressed
+        print('You Pressed the correct Key!')
+        llmmessege = llmmessege_f+random_item_from_array(read_file("llmmesseges")) # formats the varible and inserts the user selection from a list
+        print(llmmessege)
+        getllmmessege()
+
+        #break  # if user pressed a key other than the given key the loop will break
+
+
+
+
+#print(random_item_from_array(read_file("llmmesseges")))
+#print(read_file("llmmesseges"))
+#gui()
+
+#getllmmessege()
 
