f8245448c32ffdb8c070fbda9e36eeb021919731
Fizzy wizard <alex@alexmakes.net>
2024-05-02 20:39:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,80 @@
-# This is a sample Python script.
+import numpy as np
+from handwriting_synthesis import Hand
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+all_star = """Somebody once told me the world is gonna roll me
+I ain't the sharpest tool in the shed
+She was looking kind of dumb with her finger and her thumb
+In the shape of an "L" on her forehead"""
 
+downtown = """Making my way downtown
+Walking fast
+Faces pass
+And I'm home-bound"""
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+give_up = """Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you"""
 
+lines = [
+    "Father time, I'm running late",
+    "I'm winding down, I'm growing tired",
+    "Seconds drift into the night",
+    "The clock just ticks till my time expires",
+]
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    hand = Hand()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # usage demo
+    biases = [.75 for i in lines]
+    styles = [9 for i in lines]
+    stroke_colors = ['red', 'green', 'black', 'blue']
+    stroke_widths = [1, 2, 1, 2]
+
+    hand.write(
+        filename='img/usage_demo.svg',
+        lines=lines,
+        biases=biases,
+        styles=styles,
+        stroke_colors=stroke_colors,
+        stroke_widths=stroke_widths
+    )
+
+    # demo number 1 - fixed bias, fixed style
+    lines = all_star.split("\n")
+    biases = [.75 for i in lines]
+    styles = [12 for i in lines]
+
+    hand.write(
+        filename='img/all_star.svg',
+        lines=lines,
+        biases=biases,
+        styles=styles,
+    )
+
+    # demo number 2 - fixed bias, varying style
+    lines = downtown.split("\n")
+    biases = [.75 for i in lines]
+    styles = np.cumsum(np.array([len(i) for i in lines]) == 0).astype(int)
+
+    hand.write(
+        filename='img/downtown.svg',
+        lines=lines,
+        biases=biases,
+        styles=styles,
+    )
+
+    # demo number 3 - varying bias, fixed style
+    lines = give_up.split("\n")
+    biases = .2 * np.flip(np.cumsum([len(i) == 0 for i in lines]), 0)
+    styles = [7 for i in lines]
+
+    hand.write(
+        filename='img/give_up.svg',
+        lines=lines,
+        biases=biases,
+        styles=styles,
+    )
