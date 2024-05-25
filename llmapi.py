b33e36d20ec0a28ccbc25451f0f3875ad7e58410
Fizzy wizard <alex@alexmakes.net>
2024-05-25 14:02:09 +0000

    removed keyboard lib and made it a one time run script
    now using autokey to run the program with ctr+f10 on the term as it allows the the hardware interrupt of the stop button by using ctr+c too stop the machine as the Axidraw API doesn't have a stop code only a hardware pause on the main board and that's too delicate for the public to use
    
    also added a check list for on site

--- llmapi.py
+++ llmapi.py
@@ -5,9 +5,11 @@ from openai import OpenAI
 #import tkinter.messagebox as messagebox
 import keyboard
 import random
-from ttgLib.TextToGcode import ttg
+#from ttgLib.TextToGcode import ttg
 import numpy as np
 from handwriting_synthesis.hand import Hand
+from pyaxidraw import axidraw 
+import readchar
 
 
 # declaring varibles 
@@ -100,80 +102,124 @@ def split_string(text, chunk_size=73):
     return chunks
 
 
+# this function replaces the first section of the svg its dirty and hacky but works i tried edditing the xml nativly and it was not having it this will work eatch time as the rrm that genrates the hand writing doesnt change this section of the svg. im sorry but it works
+def find_replace_with_file_content(source_file_path, replacement_file_path):
+    # Read the content of the source file
+    with open(source_file_path, 'r') as source_file:
+        source_content = source_file.read()
 
+    # Read the content of the replacement file
+    with open(replacement_file_path, 'r') as replacement_file:
+        replacement_content = replacement_file.read()
 
-while True:  # making a loop
-        #keyboard.wait('q')  # if key 'q' is pressed # on the real day un comment this and remove if blar blar
-        if input()=="q":
-          print('You Pressed the correct Key!')
+    # Find the start and end indices of the substring to be replaced
+    start_index = source_content.find('<svg baseProfile')
+    end_index = source_content.find('y="0" />', start_index) + len('y="0" />')
 
-          preprompt = getLLMPrePrompt()  # Get the pre prompt
-          prompt = getLLMPrompt()  # Get the prompt
+    # Replace the substring with the content from the replacement file
+    modified_content = source_content[:start_index] + replacement_content + source_content[end_index:]
 
-          #getllmmessege(getLLMPrePrompt(),getLLMPrompt()) # passes the funcitions to the openai libary to genrate the messege
-          completion_message = getllmmessege(getLLMPrePrompt(),getLLMPrompt()) # passes the funcitions to the openai libary to genrate the messege
-
-
-          #print(type(completion_message))
-
-          #markdown_content = convert_to_markdown(completion_message.content) #.content is the messege in 
-
-          #save_to_markdown_file(markdown_content, "llmapi_output/output.md") # no longer is used
-
-
-
-          # prints the output of the LLM formated nicly 
-          print(completion_message.content)
-          #chat gpt genrated code for the next 3 lines 
-          result = split_string(completion_message.content)
-          lines = []
-          for chunk in result: # for loop to append the chuck to the last one with a max of 70 char
-            print(chunk)
-          # start the handwriting demo code
-          #lines = [completion_message.content]
-            lines.append(chunk+"")
-          #lines = ["this is a test"]
-          """"
-          biases = [.75 for _ in lines]
-          styles = [9 for _ in lines]
-          stroke_colors = ['red', 'green', 'black', 'blue']
-          stroke_widths = [1, 2, 1, 2]
-
-          hand = Hand()
-          hand.write(
-              filename='img/llmtest1.svg',
-              lines=lines,
-              biases=biases,
-              styles=styles,
-              stroke_colors=stroke_colors,
-              stroke_widths=stroke_widths
-          )
-          """
-          #fixed bias, varying style
-          #lines = downtown.split("\n")
-          biases = [.75 for i in lines]
-          styles = np.cumsum(np.array([len(i) for i in lines]) == 0).astype(int)
-          stroke_colors = ['red' for i in lines ]
-          hand = Hand()
-          hand.write(
-              filename='img/llmtest1.svg',
-              lines=lines,
-              biases=biases,
-              styles=styles,
-              stroke_colors=stroke_colors,
-          )          
-
-          # start the plot 
-          exec(open("plot.py").read())
-
-          #gcode = ttg("Text to Gcode",1,0,"return",1).toGcode("M02 S500","M05 S0","G0","G1")
-          #print(gcode)
-          #time to use the os function too convert the 
-          #os.system('pandoc output.md -o test.pdf')
-          #Penplot( getllmmessege(llmmessege,getLLMPrePrompt()))
+    # Write the modified content back to the source file
+    with open(source_file_path, 'w') as source_file:
+        source_file.write(modified_content)
 
 
 
+#key = readchar.readkey()
+# pressed the go button ( it sends a 1 key stroke )
+#while True:
+  #key = readchar.readkey()
+  #if key == "1":
+      #key = 0
+      #if keyboard.wait('1') == '1':
+      #print("You pressed the 1 key")
+
+print('You Pressed the correct Key!')
+
+preprompt = getLLMPrePrompt()  # Get the pre prompt
+prompt = getLLMPrompt()  # Get the prompt
+
+#getllmmessege(getLLMPrePrompt(),getLLMPrompt()) # passes the funcitions to the openai libary to genrate the messege
+completion_message = getllmmessege(getLLMPrePrompt(),getLLMPrompt()) # passes the funcitions to the openai libary to genrate the messege
+
+
+#print(type(completion_message))
+
+#markdown_content = convert_to_markdown(completion_message.content) #.content is the messege in
+
+#save_to_markdown_file(markdown_content, "llmapi_output/output.md") # no longer is used
+
+
+
+# prints the output of the LLM formated nicly
+print(completion_message.content)
+#chat gpt genrated code for the next 3 lines
+result = split_string(completion_message.content)
+lines = []
+for chunk in result: # for loop to append the chuck to the last one with a max of 70 char
+  print(chunk)
+# start the handwriting demo code
+#lines = [completion_message.content]
+  lines.append(chunk+"")
+#lines = ["this is a test"]
+""""
+biases = [.75 for _ in lines]
+styles = [9 for _ in lines]
+stroke_colors = ['red', 'green', 'black', 'blue']
+stroke_widths = [1, 2, 1, 2]
+
+hand = Hand()
+hand.write(
+    filename='img/llmtest1.svg',
+    lines=lines,
+    biases=biases,
+    styles=styles,
+    stroke_colors=stroke_colors,
+    stroke_widths=stroke_widths
+)
+"""
+# settings for RRN modle
+#fixed bias, varying style
+#lines = downtown.split("\n")
+biases = [.75 for i in lines]
+styles = np.cumsum(np.array([len(i) for i in lines]) == 0).astype(int)
+stroke_colors = ['red' for i in lines ]
+hand = Hand()
+hand.write(
+    filename='img/llmtest1.svg',
+    lines=lines,
+    biases=biases,
+    styles=styles,
+    stroke_colors=stroke_colors,
+)
+
+# start the plot
+exec(open("plot.py").read())
+
+#gcode = ttg("Text to Gcode",1,0,"return",1).toGcode("M02 S500","M05 S0","G0","G1")
+#print(gcode)
+#time to use the os function too convert the
+#os.system('pandoc output.md -o test.pdf')
+#Penplot( getllmmessege(llmmessege,getLLMPrePrompt()))
+#key = 0
+"""
+  #if key == "2":
+      key = 0
+      #elif keyboard.wait('2') == '2':
+      print("You pressed the 2 key")
+
+      ad = axidraw.AxiDraw()
+      ad.interactive()
+      ad.connect()
+      ad.penup()                    # raise pen
+      ad.goto(0,0)
+      ad.disconnect()
+      key = 0
+"""
+# This keeps the script running and stops it 
+  #keyboard.wait('esc')
+
+# input mode for debugging 
 
 #print(random_item_from_array(read_file("llmmesseges")))
 #print(read_file("llmmesseges"))
